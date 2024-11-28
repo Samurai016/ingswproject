4452d8e2899324a18d3304182e54f9575ebe639a
Nicolò Rebaioli <niko.reba@gmail.com>
2024-11-28 23:15:18 +0100

    Refactoring

--- src/test/java/it/unibs/ingswproject/test/logic/weight/CompleteWeightComputationTest.java
+++ src/test/java/it/unibs/ingswproject/test/logic/weight/CompleteWeightComputationTest.java
@@ -1,4 +1,4 @@
-package it.unibs.ingswproject.test.logic;
+package it.unibs.ingswproject.test.logic.weight;
 
 import it.unibs.ingswproject.logic.weight.complete.Graph;
 import it.unibs.ingswproject.logic.weight.CompleteWeightComputation;
